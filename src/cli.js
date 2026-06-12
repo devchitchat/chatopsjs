aff098e0819ec701657631044ac7f861ee8accf8
Joey Guerra <github@joeyguerra.com>
2026-06-12 15:49:09 -0500

    fix: it's not help, it's help.commands

--- src/cli.js
+++ src/cli.js
@@ -15,7 +15,7 @@ try {
     })
     robot.adapters.add(adapter)
     console.log('chatopsjs CLI')
-    console.log('Type "help" for commands, "exit" to quit')
+    console.log('Type "help.commands" for commands, "exit" to quit')
     await adapter.start()
   } else {
     const { runAdapterApp } = await loadAdapterModule(adapterSpecifier)
